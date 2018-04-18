ee4cd284beb0d117b0a3cea79f343f841673547f
Subba Reddy <asubbareddy@gmail.com>
2018-04-17 18:12:47 -0700

    Voice Me v.01

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # VoiceMe
+
+Flask Tutorial -- good one: 
+ + covers step by step of each topic : Templates, Forms, Ajax, etc..
+https://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-ii-templates
---
+++ web_server.py
@@ -0,0 +1,44 @@
+from flask import Flask, render_template, request
+
+# ----------------------------------------------------------------------
+# having a separate 'app' folder and 'router.py' in 'app' folder is causing confusion, so have a single file
+# ----------------------------------------------------------------------
+
+app = Flask(__name__, template_folder='templates')
+
+@app.route("/", methods=['GET'])
+@app.route("/hi", methods=['GET'])
+def display_form():
+        text = render_template("hi.html")
+        return text
+
+
+@app.route("/hi", methods=['POST'])
+def check_data():
+        data = request.form  # this came from web browser in the form of dictionary
+        name = data['name']
+        age = int(data['age'])
+        user_data = {'name': name, age: age}
+
+        #message = check_age(age)
+        if age < 18:
+            message = "sorry, you have to wait till 18 to drive"
+        else:
+            message = "Hurry!!!!!!, you can drive"
+
+        text = render_template("hi_response.html",
+                               target="handle_hi",
+                               name = name,
+                               message = message)
+        return text
+
+
+@app.route("/bye")
+def bye():
+    text = render_template("bye.html")
+    return text
+
+
+if __name__ == "__main__":
+    app.run(debug=True)   # this debug flag detects "code file chagnes & start Flash server automatically"
+    #app.run()
